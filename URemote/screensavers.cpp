473ebe859a2f52ed44b53ece0d7ea6178b34c92b
Greg Cunningham <greg@curioustech.net>
2024-04-01 23:24:59 -0400

    Replaced a screensaver

--- URemote/screensavers.cpp
+++ URemote/screensavers.cpp
@@ -15,8 +15,8 @@ void ScreenSavers::select(int n)
     case SS_Lines:
         Lines(true);
         break;
-    case SS_Boing:
-        Boing(true);
+    case SS_Bubbles:
+        Bubbles(true);
         break;
   }
 }
@@ -26,7 +26,7 @@ void ScreenSavers::run()
   switch(m_saver)
   {
     case SS_Lines: Lines(false); break;
-    case SS_Boing: Boing(false); break;
+    case SS_Bubbles: Bubbles(false); break;
   }
 
   if(display.m_bCharging)
@@ -68,6 +68,7 @@ void ScreenSavers::Lines(bool bInit)
   }
 
   uint8_t next = (idx + 1) % LINES;
+
   tft.drawLine(line[next].x1, line[next].y1, line[next].x2, line[next].y2, TFT_BLACK);
 
   // add delta to positions
@@ -98,118 +99,59 @@ void ScreenSavers::Lines(bool bInit)
   idx = next;
 }
 
-void ScreenSavers::Boing(bool bInit)
+void ScreenSavers::Bubbles(bool bInit)
 {
-  const int16_t rad = 12;
-  static Ball *ball = (Ball *)m_buffer;
+  static Bubble *bubble = (Bubble *)m_buffer;
   static uint8_t skipper = 4;
 
+  if(bInit)
+  {
+    tft.fillScreen(TFT_BLACK);
+    for(uint8_t i = 0; i < BUBBLES; i++)
+    {
+      initBubble(bubble[i]);
+    }
+    return;
+  }
+
+  if(--skipper) // slow it down by x*loop delay of 1ms
+    return;
+  skipper = 12;
+
+  // Erase last bubble
+  for(uint8_t i = 0; i < BUBBLES; i++)
+  {
+    tft.drawCircle(bubble[i].x, bubble[i].y, bubble[i].size, TFT_BLACK );
+    if(bubble[i].size > random(25, 8))
+    {
+      initBubble(bubble[i]);
+    }
+  }
+
+  // Draw bubbles
+  for(uint8_t i = 0; i < BUBBLES; i++)
+  {
+    bubble[i].size += bubble[i].is;
+    bubble[i].x += bubble[i].dx;
+    bubble[i].y += bubble[i].dy;
+    bubble[i].x = constrain(bubble[i].x, 1, DISPLAY_WIDTH-1);
+    bubble[i].y = constrain(bubble[i].y, 1, DISPLAY_HEIGHT-1);
+    tft.drawCircle(bubble[i].x, bubble[i].y, bubble[i].size, bubble[i].color );
+  }
+}
+
+void ScreenSavers::initBubble(Bubble& bubble)
+{
   const uint16_t palette[] = {TFT_MAROON,TFT_PURPLE,TFT_OLIVE,TFT_LIGHTGREY,
         TFT_DARKGREY,TFT_BLUE,TFT_GREEN,TFT_CYAN,TFT_RED,TFT_MAGENTA,
         TFT_YELLOW,TFT_WHITE,TFT_ORANGE,TFT_GREENYELLOW,TFT_PINK,
         TFT_GOLD,TFT_SILVER,TFT_SKYBLUE,TFT_VIOLET};
 
-  if(bInit)
-  {
-    tft.fillScreen(TFT_BLACK);
-    for(uint8_t i = 0; i < BALLS; i++)
-    {
-      ball[i].x = DISPLAY_WIDTH/2;
-      ball[i].y = rad;
-      ball[i].dx = (int16_t)random(-2, 5);
-      ball[i].dy = (int16_t)random(3, 6);
-      ball[i].color = palette[ random(0, sizeof(palette) / sizeof(uint16_t) ) ];
-    }
-    return;
-  }
-
-  if(--skipper) // slow it down by x*loop delay of 2ms
-    return;
-  skipper = 16;
-
-  static double  bounce = 1.0;
-  static double  energy = 0.6;
-
-  // bouncy
-  for(uint8_t i = 0; i < BALLS; i++)
-  {
-    int x1 = ball[i].x;
-    int y1 = ball[i].y;
-
-    // check for wall collision
-    if(y1 <= rad && ball[i].dy < 0)   // top of screen
-      ball[i].dy = -ball[i].dy + (int16_t)random(0, 4);
-
-    if(y1 >= DISPLAY_HEIGHT - rad && ball[i].dy > 0) // bottom
-      ball[i].dy = -ball[i].dy - (int16_t)random(5, 17);
-
-    if(x1 <= rad && ball[i].dx < 0)  // left wall
-      ball[i].dx = -ball[i].dx;
-
-    if(x1 >= DISPLAY_WIDTH - rad && ball[i].dx > 0)  // right wall
-      ball[i].dx = -ball[i].dx;
-
-    static uint8_t dly = 1;
-    if(--dly == 0) // gravity
-    {
-      ball[i].dy++;
-      dly = 1;
-    }
-    if(ball[i].dx < 2 && ball[i].dx > -2)
-      ball[i].dx += (int16_t)random(-2, 4); // keep balls from sitting still
-
-    if(ball[i].dy < 2 && ball[i].dy > -2)
-      ball[i].dy += (int16_t)random(-2, 4); // keep balls from sitting still
-
-    // check for ball to ball collision
-    for(uint8_t i2 = i+1; i2 < BALLS; i2++)
-    {
-      double n, n2;
-      int x2, y2;
-  
-      x2 = ball[i2].x;
-      y2 = ball[i2].y;
-      if(x1 + rad >= x2 && x1 <= x2 + rad && y1 + rad >= y2 && y1 <= y2 + rad){
-        if(x1-x2 > 0){
-          n = ball[i].dx;
-          n2 = ball[i2].dx;
-          ball[i].dx = ((n *bounce) + (n2 * energy));
-          ball[i2].dx = -((n2 * bounce) - (n*energy));
-        }else{
-          n = ball[i].dx;
-          n2 = ball[i2].dx;
-          ball[i].dx = -((n * bounce) - (n2 * energy));
-          ball[i2].dx = ((n2 * bounce) + (n * energy));
-        }
-        if(y1-y2 > 0){
-          n = ball[i].dy;
-          ball[i].dy = ((n * bounce) + (ball[i2].dy * energy));
-          ball[i2].dy = -((ball[i2].dy * bounce) - (n * energy));
-        }else{
-          n = ball[i].dy;
-          ball[i].dy = -((n * bounce) - (ball[i2].dy * energy));
-          ball[i2].dy = ((ball[i2].dy * bounce) + (n * energy));
-        }
-      }
-    }
-
-    ball[i].dx = constrain(ball[i].dx, -20, 40); // speed limit
-    ball[i].dy = constrain(ball[i].dy, -20, 40);
-  }
-
-  // Erase last balls
-  for(uint8_t i = 0; i < BALLS; i++)
-  {
-    tft.drawCircle(ball[i].x, ball[i].y, rad, TFT_BLACK );
-  }
-  
-  // Draw balls
-  for(uint8_t i = 0; i < BALLS; i++)
-  {
-    ball[i].x += ball[i].dx;
-    ball[i].y += ball[i].dy;
-
-    // Draw at new positions
-    tft.drawCircle(ball[i].x, ball[i].y, rad, ball[i].color );
-  }
+  bubble.x = (int16_t)random(2, DISPLAY_WIDTH - 2);
+  bubble.y = (int16_t)random(2, DISPLAY_WIDTH - 2);
+  bubble.dx = (int8_t)random(-2, 4);
+  bubble.dy = (int8_t)random(-2, 4);
+  bubble.size = 1;
+  bubble.is = random(1, 3);
+  bubble.color = palette[ random(0, sizeof(palette) / sizeof(uint16_t) ) ];
 }
