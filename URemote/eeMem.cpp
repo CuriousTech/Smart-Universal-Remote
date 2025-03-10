176dcc87cc3e4823628706d520013778724b69db
Greg Cunningham <greg@curioustech.net>
2025-03-09 22:50:21 -0400

    Changed from file to prefs

--- URemote/eeMem.cpp
+++ URemote/eeMem.cpp
@@ -1,66 +1,49 @@
 #include "eeMem.h"
-#include <FS.h>
-#include <FFat.h>
+
+#include <Preferences.h>
+Preferences prefs;
 
 void eeMem::init()
+{
+  prefs.begin("my-app", false);
+  verify(false);
+}
+
+bool eeMem::update() // write the settings if changed
+{
+  uint16_t old_sum = ee.sum;
+  ee.sum = 0;
+  ee.sum = Fletcher16((uint8_t*)this + offsetof(eeMem, size), EESIZE);
+
+  if(old_sum == ee.sum)
+  {
+    return false; // Nothing has changed?
+  }
+
+  uint8_t *pData = (uint8_t *)this + offsetof(eeMem, size);
+  prefs.putBytes("Config", pData, EESIZE);
+  return true;
+}
+
+bool eeMem::verify(bool bComp)
 {
   uint8_t data[EESIZE];
   uint16_t *pwTemp = (uint16_t *)data;
 
-  m_bFsOpen = FFat.begin(true);
-
-  if(!m_bFsOpen)
-  {
-    ets_printf("FATFS failed\r\n");
-    return;
-  }
-
-  File F = FFat.open("/eemem.bin", "r");
-  if(F)
-  {
-    F.read(data, EESIZE);
-    F.close();
-  }
+  prefs.getBytes("Config", data, EESIZE);
   if(pwTemp[0] != EESIZE)
-    return; // revert to defaults if struct size changes
+    return false; // revert to defaults if struct size changes
 
   uint16_t sum = pwTemp[1];
   pwTemp[1] = 0;
   pwTemp[1] = Fletcher16(data, EESIZE );
   if(pwTemp[1] != sum)
-    return; // revert to defaults if sum fails
+    return false; // revert to defaults if sum fails
+
+  if(bComp)
+    return ( !memcmp(this + offsetof(eeMem, size), data, EESIZE ) ); // don't load
   memcpy(this + offsetof(eeMem, size), data, EESIZE );
-}
-
-bool eeMem::update() // write the settings if changed
-{
-  if(!check() || m_bFsOpen == false) // make sure sum is correct, and if it can save
-    return false;
-
-  nWriteCnt++;
-  check(); // recalc sum
-
-  File F = FFat.open("/eemem.bin", "w");
-  if(F)
-  {
-    F.write((byte*) this + offsetof(eeMem, size), EESIZE);
-    F.close();
-    return true;
-  }
-  return false;
-}
-
-bool eeMem::check()
-{
-  uint16_t old_sum = ee.sum;
-  ee.sum = 0;
-  ee.sum = Fletcher16((uint8_t*)this + offsetof(eeMem, size), EESIZE);
-  return (old_sum == ee.sum) ? false:true;
-}
-
-uint16_t eeMem::getSum()
-{
-  return Fletcher16((uint8_t*)this + offsetof(eeMem, size), EESIZE);
+  return true;
 }
 
 uint16_t eeMem::Fletcher16( uint8_t* data, int count)
@@ -73,6 +56,5 @@ uint16_t eeMem::Fletcher16( uint8_t* data, int count)
       sum1 = (sum1 + data[index]) % 255;
       sum2 = (sum2 + sum1) % 255;
    }
-
    return (sum2 << 8) | sum1;
 }
